4ead2fd1d5bd71c79467c799f8696c41692c26a9
Jimmi Elofsson <jimmi.elofsson@hyperisland.se>
2014-04-14 23:33:22 +0200

    ändrad:     app/rest.php

--- app/rest.php
+++ app/rest.php
@@ -3,9 +3,15 @@
 class REST extends Controller {
 	  
 	function beforeroute($app) {  
-		$app->set('menu', array());
-		$app->set('usermenu', array()); 
-		$app->set('breadcrumbs',array()); 
+        $app->set('apiCall', true);
+        //  -------------------------------------------
+        // Set default API REST Headers
+        // Returning JSON NO CACHE
+        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+        header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' );
+        header('Content-type: application/json');
+        header('Pragma: no-cache');
+        header('Cache-Control: no-store, no-cache, must-revalidate');
 	}
 	 
 	function show($app, $args){
